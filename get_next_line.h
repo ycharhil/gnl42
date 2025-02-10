d1d4e4b6d70e411ffce31b475ac82660e92cd1d3
Yacine Charhil <ycharhil@paul-f3Br1s1.clusters.42paris.fr>
2025-02-10 16:53:03 +0100

    push

--- get_next_line.h
+++ get_next_line.h
@@ -5,14 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ycharhil <ycharhil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/30 13:01:49 by ycharhil          #+#    #+#             */
-/*   Updated: 2025/02/03 14:24:43 by ycharhil         ###   ########.fr       */
+/*   Created: 2025/02/07 16:09:59 by ycharhil          #+#    #+#             */
+/*   Updated: 2025/02/10 15:15:26 by ycharhil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -20,9 +23,11 @@
 
 char	*get_next_line(int fd);
 char	*ft_strdup(char *s);
-size_t	ft_strlen(char *s);
-char    *ft_substr(char *s, unsigned int start, size_t len);
-char    *ft_strjoin(char *s1, char *s2);
-void    fill_str(char *res, char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
 
-# endif
+size_t	ft_strlen(char *s);
+
+void	fill_str(char *res, char *s1, char *s2);
+
+#endif
